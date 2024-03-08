fdf1e6de1c86dc97c8aabee102a6c893e889833b
Caio César Faria <131374603+CaioCesarFaria@users.noreply.github.com>
2024-03-08 15:56:22 -0300

    Add files via upload

---
+++ script/hamburguer.js
@@ -0,0 +1,10 @@
+// ! MENU HAMBURGUER
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerMenu = document.getElementById("menu_hamburguer");
+  const menu = document.getElementById("menu");
+
+  hamburgerMenu.addEventListener("click", function () {
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+  });
+});
